7040753276e9fe0aa83b596fea2e1d5b222984c3
kimseoyun98 <seoyunkim98@gmail.com>
2024-09-02 19:18:45 +0900

    💄 Style:
    백업이미지 추가, Enlarged 모달 기능 추가, 이미지 크기에 비례한 min, max 사이즈 지정

--- src/pages/DetailPost.jsx
+++ src/pages/DetailPost.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { PostContext } from '../context/PostContextProvider';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
@@ -6,8 +6,11 @@ import '../globalStyle.css';
 
 const DetailPost = () => {
   const { id } = useParams();
-  const { posts, deletePosts } = useContext(PostContext); // deletePost 함수 가져오기
-  const navigate = useNavigate(); // useNavigate 훅 사용
+  const { posts, deletePosts } = useContext(PostContext);
+  const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
 
   // user 가 특정되면 주석 풀기
   // if (!user) {
@@ -43,23 +46,11 @@ const DetailPost = () => {
     <>
       <S_PostSectionWrapper>
         <S_PostSection>
-          <div>
+          <S_TagSection>
             {post.tech_stack.map((tech, index) => (
-              <span
-                key={index}
-                style={{
-                  fontSize: '0.8rem',
-                  display: 'inline-block',
-                  padding: '6px 10px',
-                  border: '1px solid #9F9F9F',
-                  borderRadius: '100px',
-                  marginRight: '8px'
-                }}
-              >
-                #{tech}
-              </span>
+              <S_Tag key={index}>#{tech}</S_Tag>
             ))}
-          </div>
+          </S_TagSection>
           <h1
             style={{
               fontSize: '2.5rem',
@@ -91,17 +82,22 @@ const DetailPost = () => {
             </S_PostInfoBar>
           </S_PostInfoBarWrapper>
           <img
-            src={post.thumbnail_url}
+            src={post.thumbnail_url ? post.thumbnail_url : '/no-img.png'}
             alt={post.title}
-            style={{ fontSize: '0.8rem', maxWidth: '80%', height: 'auto' }}
+            style={{ fontSize: '0.8rem', maxWidth: '100%', height: 'auto', maxHeight: '400px', cursor: 'pointer' }}
+            onClick={openModal}
           />
-
+          {isModalOpen && (
+            <S_ModalOverlay onClick={closeModal}>
+              <StyledImage src={post.thumbnail_url} alt="썸네일 자세히보기" />
+            </S_ModalOverlay>
+          )}
           <S_PostInfoTimeWrapper>
             <p style={{ fontWeight: '200', fontSize: '0.8rem' }}>
               {post.project_start_date} ~ {post.project_end_date}
             </p>
           </S_PostInfoTimeWrapper>
-          <p style={{ fontWeight: '400', fontSize: '1.2rem', lineHeight: '1.6' }}>{post.content}</p>
+          <p style={{ fontWeight: '400', fontSize: '1rem', lineHeight: '1.6' }}>{post.content}</p>
         </S_PostSection>
       </S_PostSectionWrapper>
     </>
@@ -123,23 +119,37 @@ const S_PostSection = styled.section`
   flex-direction: column;
   align-items: center;
   gap: 2rem;
-  width: 50%;
+  width: 80%;
+`;
+
+const S_TagSection = styled.section`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.5rem;
+`;
+const S_Tag = styled.span`
+  font-size: 0.8rem;
+  display: block;
+  padding: 6px 10px;
+  border: 1px solid #9f9f9f;
+  border-radius: 100px;
+  white-space: nowrap;
 `;
 
 const S_PostInfoBarWrapper = styled.div`
   display: flex;
   align-items: center;
   border-bottom: 1px solid #9f9f9f;
-
   width: 100%;
+  margin-top: -0.5rem;
 `;
 const S_PostInfoBar = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  padding: 18px 16px;
+  padding: 10px 4px;
   justify-content: space-between;
-
   width: 100%;
 `;
 const S_PostInfoActionBar = styled.div`
@@ -147,6 +157,27 @@ const S_PostInfoActionBar = styled.div`
   align-items: center;
   justify-content: end;
   gap: 1rem;
+`;
+
+const S_ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
+const StyledImage = styled.img`
+  min-width: 300px;
+  max-width: 80vw;
+  min-height: 400px;
+  max-height: 80vh;
+  height: auto;
+  object-fit: contain;
 `;
 
 const S_PostInfo = styled.p`
