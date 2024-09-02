1afd9e1d54506833be05818c0b12f7304950615c
wldud7788 <rooftop7788@naver.com>
2024-09-02 17:14:03 +0900

    💄 Add: 헤더,푸터 css 수정
    1. 헤더,푸터 색상 수정

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,14 @@
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import { Outlet } from 'react-router-dom';
+import { S_OutletConatiner } from '../styled/StyledHome.js';
 const Layout = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <S_OutletConatiner>
+        <Outlet />
+      </S_OutletConatiner>
       <Footer />
     </>
   );
