0242fcd90069842cf313c09f4253debc713a56f9
jeongol <jungcode127@gmail.com>
2024-09-03 11:15:43 +0900

    🐛 bugfix: 버그 수정후 저장

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,28 @@ import Router from './shared/Router';
 import { S_AppContainer } from './styled/StyledHome';
 
 function App() {
+  window.addEventListener('load', async () => {
+    const sessionExpired = localStorage.getItem('sessionExpired');
+    if (sessionExpired) {
+      const { error } = await supabase.auth.signOut(); // 로그아웃 처리
+      if (error) {
+        console.error('로그아웃 중 오류 발생:', error);
+      } else {
+        console.log('세션이 삭제되었습니다.');
+      }
+      localStorage.removeItem('sessionExpired'); // 상태 제거
+    }
+  });
+
+  window.addEventListener('beforeunload', () => {
+    localStorage.removeItem('sb-sdvgajhhvhmnjzrmwkbt-auth-token');
+    const { error } = supabase.auth.signOut(); // 로그아웃 처리
+    if (error) {
+      console.error('로그아웃 중 오류 발생:', error);
+    } else {
+      console.log('세션이 삭제되었습니다.');
+    }
+  });
   return (
     <S_AppContainer>
       <UserContextProvider>
