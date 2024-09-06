cb4e7a2c17d03982891e7ad46dd4b2b9e92f5150
minyeong <minyk0512@gmail.com>
2024-09-06 10:13:06 +0900

    🐛 bugfix: 브라우저 탭 이동시 supabase session 연결이 끊기는 버그 해결

--- src/context/UserContextProvider.jsx
+++ src/context/UserContextProvider.jsx
@@ -39,7 +39,24 @@ export const UserContextProvider = ({ children }) => {
 
     getSession();
 
+    const atChangeVisible = async () => {
+      if (document.visibilityState === 'visible') {
+        window.location.reload();
+      }
+    };
+
+    document.removeEventListener('visibilitychange', atChangeVisible);
+    document.addEventListener('visibilitychange', atChangeVisible);
+
     const { data: authListener } = supabase.auth.onAuthStateChange(async (event, session) => {
+      const currentTime = Math.floor(Date.now() / 1000);
+      if (session?.expires_at < currentTime) {
+        console.log('시간초과: 세션이 만료되었습니다.');
+        setUser(null); // 로그아웃 처리
+        window.location.reload();
+        return;
+      }
+
       if (session?.user) {
         const { id, email, created_at } = session.user;
 
@@ -61,6 +78,7 @@ export const UserContextProvider = ({ children }) => {
     });
 
     return () => {
+      document.removeEventListener('visibilitychange', atChangeVisible);
       authListener.subscription.unsubscribe();
     };
   }, []);
